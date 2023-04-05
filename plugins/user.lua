e99dbd009a111096f6cb2b3684a6ada227bd8376
Tej Chajed <tchajed@gmail.com>
2023-04-04 17:22:04 -0700

    Add lsp keybindings

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,5 +1,7 @@
 return {
   "Th3Whit3Wolf/one-nvim",
+  { "mlr-msft/vim-loves-dafny", ft="dafny" },
+  { "whonore/Coqtail", ft="coq" },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
